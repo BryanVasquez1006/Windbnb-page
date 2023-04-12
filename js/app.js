abfda93e07b74c1e36524ba3357fc6377de6c5cf
BryanVasquez1006 <bryan.vasqueziraheta@gmail.com>
2023-04-12 17:01:24 -0600

    Funciones de cards agregadas al proyecto

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
+import data from "./data.js";
+import dom from "./dom.js";
+
+const URL = "./stays.json";
+const rooms = dom.$("#rooms")
+
+
+const datos = await data.getData(URL)
+datos.forEach(element => {
+    //Se crea el card con la informacion del elemento
+    const card = dom.newCard(element);
+
+    //Se agrega el card al elemento rooms
+    rooms.appendChild(card)
+
+})
 
--- js/dom.js
+++ js/dom.js
@@ -2,3 +2,45 @@
 Aqui van todas las funciones o variables relacionadas 
 con la manipulación del DOM en la aplicación
 */
+
+
+
+const $ = (selector) => document.querySelector(selector)
+
+const newCard = (obj)=> {
+    const div = document.createElement("div");
+    div.className = "card-img";
+
+    //Aqui se insertan los elementos que tiene el card
+
+    div.innerHTML = `
+    <div class="card-img row border border-white col-1 d-flex flex-column align-content-center align-item " >
+        <img src="${obj.photo}" class="p-1 w-100" alt="${obj.city}">
+
+
+    <div class="card-info d-flex flex-row justify-content-between  ">
+      <span class="super-host d-flex flex-column justify-content-center border border-2 rounded-4 border-black ">
+        <p class=" container-fluid fs-6 fw-bold">SUPER HOST</p>
+      </span>
+      <p class=" fs-5 p-cardInfo">${obj.type} . ${obj.beds} beds</p>
+      <div class="d-flex flex-row  ">
+        <span class="star material-symbols-outlined">
+          star
+          </span>
+        <p class=" fs-5 ranting p-cardInfo ">${obj.rating}</p>
+      </div>
+    </div>
+    <div class="city-description d-flex flex-row">
+      <p>${obj.title}</p>
+     </div>
+
+  </div>
+    `
+
+    return div;
+}
+
+export default {
+    newCard,
+    $
+}

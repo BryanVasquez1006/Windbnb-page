31c00970184a8632f21158e8fad4d66e69e6a5e1
BryanVasquez1006 <bryan.vasqueziraheta@gmail.com>
2023-07-31 23:23:09 -0600

    Filters parcially fixed

--- js/app.js
+++ js/app.js
@@ -5,132 +5,164 @@ import dom from "./dom.js";
 
 const URL = "./stays.json";
 const rooms = dom.$("#rooms");
-const staysIn = dom.$("#staysIn")
+const staysIn = dom.$("#stays");
 const guestsInformation = document.getElementById("options");
-const location = document.getElementById("locationInformation");
+const location = dom.$("#locationInformation");
 const guests = document.getElementById("guestsInformation");
-const buscarBtn = document.getElementById("buscar")
+const buscarBtn = document.getElementById("buscar");
 const locations = document.getElementById("locationNames");
-const substractBtn = document.getElementById("restarBtn")
+const substractBtn = document.getElementById("restarBtn");
 const addBtn = document.getElementById("sumarBtn");
-const addingGuests = document.getElementById("counter")
-const restarChildren = document.getElementById("restarChildren")
-const sumarChildren = document.getElementById("sumarChildren")
-const contarChildren = document.getElementById("counterChildren")
-console.log(contarChildren, sumarChildren, restarChildren)
+const addingGuests = document.getElementById("counter");
+const addingChildren = document.getElementById("counterChildren");
+const restarChildren = document.getElementById("restarChildren");
+const sumarChildren = document.getElementById("sumarChildren");
+const contarChildren = document.getElementById("counterChildren");
+const locationButtons = document.querySelectorAll(".locationBTN");
 
+// console.log(contarChildren, sumarChildren, restarChildren);
 
+// let locationName = "";
 
-let locationName = "";
-let guestsAmount = 0;
-
-const datos = await data.getData(URL);
-const categories =  data.getCategories(datos);
-
-
-
-
-
-
-
-
-
-
-//HACER QUE LOS BOTONES INSERTEN EL NUMERO DE GUESTS 
-let minimoContador = 0
+let guestsTotal = 0;
+let selectedCity = "";
+let minimoContador = 0;
+let maximoContadorChild = 13;
+let minimoContadorChild = 0;
+let valorContadorChild = 0;
 let maximoContador = 13;
 let valorContador = 0;
 
+const datos = await data.getData(URL);
+const categories = data.getCategories(datos);
 
-const sumar = addBtn.addEventListener("click", (e) => {
-    if (valorContador < maximoContador ) {
-        valorContador++
-addingGuests.textContent = valorContador
-console.log(addBtn)
-    }
-
-})
-
-const restar = substractBtn.addEventListener("click", (e) => {
-    if (valorContador > minimoContador ) {
-        valorContador--;
-        addingGuests.textContent = valorContador
-        console.log()
-      }
-
-    })
-
-//  restar(restarChildren)
-// sumar(sumarChildren)
-
-
- //FIN DE HACER QUE LOS BOTONES INSERTEN EL NUMERO DE GUESTS 
+// FUNCION QUE INSERTA LAS CARDS
+function showcards(stays) {
+    rooms.innerHTML = "";
+    stays
+      .filter((element) => {
+        const cityName = element.city.toLowerCase();
+        const guestsCount = parseInt(guests.value); // se obtiene el número de huéspedes ingresado
+        const cardGuests = parseInt(element["data-guests"]); 
+        // Aplica los filtros de ciudad y huéspedes
+        return (selectedCity === "" || cityName === selectedCity) && (isNaN(guestsCount) || guestsCount <= cardGuests);
+      })
+      .forEach((element) => {
+        // Se crea el card con la información del elemento
+        const card = dom.newCard(element);
+        // Se agrega el card al elemento rooms
+        rooms.appendChild(card);
+        console.log(card);
+      });
+  }
 
 
 
+// FUNCION PARA FILTRAR LA BUSQUEDA POR CIUDAD
 
+locationButtons.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    e.preventDefault();
+    const nombreCiudad = getCityName(
+      this.querySelector(".countryList").textContent
+    );
+    const cityName = this.dataset.city;
+    location.value = cityName;
+    selectedCity = nombreCiudad;
 
-
-//HACER QUE LOS PAISES DESAPAREZCAN 
-location.addEventListener("click",(e) => {
-   
-    if (locations.style.display == "none") {
-
-        locations.style.display == "block"
-    }else{
-        locations.style.display == "none"
-        console.log(locations)
-      
-    } 
-}
-)
-
-
-
-
+    showcards(datos);
+    staysCounter();
+    
+  });
+});
 
 //AGREGANDO EL EVENTO CLICK AL INPUT DE GUESTS
-guests.addEventListener("keyup", (e) => {
-guestsAmount = parseInt(guests.value);
-console.log(guestsAmount)
-})
+// guests.addEventListener("keyup", (e) => {
+//   guestsAmount = parseInt(guests.value);
+//   console.log(guestsAmount);
+// });
 
-//INICIA FILTRO PARA LOCATION Y GUESTS
 location.addEventListener("keyup", (e) => {
-locationName = location.value;
-console.log(locationName)
-})
-
-buscarBtn.addEventListener("click", (e) => {
-
-    // let guestsNumber = parseInt(guests.value);
-    let space = datos.filter(dato => dato.city.toLowerCase().includes(locationName.toLowerCase()) && dato.maxGuests <= guestsAmount)
-
-    showcards(space)
-    console.log(space)
-
+    const locationName = location.value.toLowerCase();
+    const cards = rooms.children;
   
+    for (const card of cards) {
+      const countryName = card.getAttribute("data-city").toLowerCase(); // Cambia 'location' por "data-location"
+      const guestsCount = parseInt(guests.value); // Obtiene el número de huéspedes ingresado
+      const cardGuests = parseInt(card.getAttribute("data-guests")); // Asume que cada card tiene un atributo 'data-guests' que contiene el número de huéspedes permitido
+  
+      // Aplica los filtros de ciudad y huéspedes
+      if (countryName.includes(locationName) && (isNaN(guestsCount) || guestsCount <= cardGuests)) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    }
+    
+    // Actualiza las cards en función del filtro de ciudad y huéspedes
+    const cityName = locationName.trim();
+    showcards(datos, cityName);
+  });
 
+// FUNCION PARA MOSTRAR CUANTAS HABITACIONES HAY DISPONIBLES
 
-}) 
-//TERMINA FILTRO PARA LOCATION
-
-
-
-
-
-
-function showcards (stays) {
-    rooms.innerHTML = ""
-    stays.forEach(element => {
-    //Se crea el card con la informacion del elemento
-        const card = dom.newCard(element);
-    //Se agrega el card al elemento rooms
-         rooms.appendChild(card)
-
-    })
+function staysCounter() {
+  //obteniendo el total de elementos hijos de mi elemento padre "rooms" =14
+  const numOfStays = rooms.children.length;
+  staysIn.innerHTML = numOfStays <= 14 ? numOfStays : "14+";
 }
-showcards(datos)
+staysCounter();
 
+function getCityName(name) {
+  const nombreCiudad = name.split(",")[0].trim();
+  console.log(nombreCiudad);
+  return nombreCiudad;
+}
 
+//HACER QUE LOS BOTONES INSERTEN EL NUMERO DE GUESTS
 
+const sumar = addBtn.addEventListener("click", () => {
+  if (valorContador < maximoContador) {
+    valorContador++;
+    actualizarContador(valorContador, addingGuests);
+  }
+});
+
+const restar = substractBtn.addEventListener("click", () => {
+  if (valorContador > minimoContador) {
+    valorContador--;
+
+    actualizarContador(valorContador, addingGuests);
+  }
+});
+
+const addChilldren = sumarChildren.addEventListener("click", () => {
+    if (valorContadorChild < maximoContadorChild) {
+      valorContadorChild++;
+  
+      actualizarContador(valorContadorChild, addingChildren);
+    }
+  });
+  
+  const substractChildren = restarChildren.addEventListener("click", () => {
+    if (valorContadorChild > minimoContadorChild) {
+      valorContadorChild--;
+  
+      actualizarContador(valorContadorChild, addingChildren);
+    }
+  });
+
+function actualizarContador(valor, elemento) {
+  elemento.textContent = valor;
+  console.log(valor);
+}
+
+//TERMINA FILTRO PARA LOCATION
+buscarBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const locationName = location.value.trim().toLowerCase();
+  selectedCity = locationName !== "" ? locationName : "";
+  staysCounter();
+  
+  showcards(datos);
+});
--- js/dom.js
+++ js/dom.js
@@ -2,35 +2,31 @@
 Aqui van todas las funciones o variables relacionadas 
 con la manipulación del DOM en la aplicación
 */
+import data from "./data.js";
+const $ = (selector) => document.querySelector(selector);
 
+let id = "";
+const newCard = (obj) => {
+  const div = document.createElement("div");
+  div.className = "card-img col";
 
+  div.classList.add(obj.city);
 
-
-
-
-
-
-
-
-
-
-const $ = (selector) => document.querySelector(selector)
-
-const newCard = (obj)=> {
-    const div = document.createElement("div");
-    div.className = "card-img col";
-
-    //Aqui se insertan los elementos que tiene el card
-    div.innerHTML = `
+  //Aqui se insertan los elementos que tiene el card
+  div.innerHTML = `
     
-    <div class=" card-img card-img-modified card border border-0  d-flex flex-wrap align-content-center">
+    <div class=" card-img card-img-modified card border border-0  d-flex flex-wrap align-content-center"  >
       <div class="test">
           <img src="${obj.photo}" class="pb-3 w-100 h-100" alt="${obj.city}">
       </div>
       <div class="card-info d-flex flex-row justify-content-between">
         <div class="chanchito gap-4 w-100 d-flex justify-content-between ">
-          <h1 class="${!obj.superHost ? "d-none" : ''} superHost fw-bold text-center rounded-pill ">SUPER HOST</h1>
-          <p class=" p-cardInfo">${obj.type}  ${obj.beds >=1 ? `. ${obj.beds} beds`: ""}</p>
+          <h1 class="${
+            !obj.superHost ? "d-none" : ""
+          } superHost fw-bold text-center rounded-pill ">SUPER HOST</h1>
+          <p class=" p-cardInfo">${obj.type}  ${
+    obj.beds >= 1 ? `. ${obj.beds} beds` : ""
+  }</p>
           <p class="d-flex  ">
             <span class="star material-symbols-outlined">star</span>
             <span class="  ranting p-cardInfo ">${obj.rating}</span>
@@ -41,16 +37,16 @@ const newCard = (obj)=> {
         <h6 class = text-nowrap>${obj.title}</h6>
       </div>
     </div>
-    `
-    return div;
-}
+    `;
 
+  return div;
+};
 
 
 
 
 export default {
-    newCard,
-    $
-}
-
+  newCard,
+  $,
+  
+};
